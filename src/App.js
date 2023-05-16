4b90c82ce27c8c77426556b0e9a51b1a53e029b7
Serhii <sergeyhall@gmail.com>
2023-05-16 21:41:16 +0200

    working code

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import '@tomtom-international/web-sdk-maps/dist/maps.css';
 import {useEffect, useRef, useState } from "react";
 import * as tt from '@tomtom-international/web-sdk-maps'
+import * as ttapi from '@tomtom-international/web-sdk-services'
 
 const App = () => {
   const apiKey = process.env.REACT_APP_TOM_TOM_API_KEY;
@@ -12,7 +13,51 @@ const App = () => {
     const [longitude, setLongitude] = useState(21.0122);
     const [latitude, setLatitude] = useState(52.2297);
 
+    const convertToPoints = (lngLat) => {
+        return {
+            point: {
+                longitude: lngLat.lat,
+                latitude: lngLat.lng
+            }
+        }
+    }
+
+    const drawRoute = (geoJson, map) => {
+        if (map.getLayer('route')) {
+            map.removeLayer('route');
+            map.removeSource('route');
+        }
+        map.addLayer({
+            id: 'route',
+            type: 'line',
+            source: {
+                type: 'geojson',
+                data: geoJson
+            },
+            paint: {
+                'line-color': '#4a90e2',
+                'line-width': 6
+            }
+        })
+    };
+
+    const addDeliveryMarker = (lngLat, map) => {
+        const element = document.createElement('div');
+        element.className = 'marker-delivery';
+        new tt.Marker({
+            element: element
+        })
+            .setLngLat(lngLat)
+            .addTo(map);
+    };
+
 useEffect(() => {
+  const origin = {
+        lng: longitude,
+        lat: latitude
+  }
+    const destinations = [];
+
   let map = tt.map({
     key: apiKey,
     container: mapElement.current,
@@ -43,9 +88,62 @@ useEffect(() => {
         setLongitude(lngLat.lng);
         setLatitude(lngLat.lat);
     });
+
+    marker.setPopup(popup).togglePopup();
   };
 
   addMarker();
+
+  const sortDestinations = (locations) => {
+      const pointsForDestinations = locations.map((destination) => {
+          return convertToPoints(destination)
+      })
+      const callParams = {
+          key: apiKey,
+          destinations: pointsForDestinations,
+          origins: [convertToPoints(origin)]
+      }
+      return new Promise((resolve, reject) => {
+          ttapi.services.matrixRouting(callParams)
+              .then((matrixAPIResults) => {
+                    const results = matrixAPIResults.matrix[0];
+                    const resultsArray = results.map((result, index) => {
+                        return {
+                            location: locations[index],
+                            drivingtime: result.response.summary?.travelTimeInSeconds || 0,
+                        }
+                    })
+                    resultsArray.sort((a, b) => {
+                        return a.drivingtime - b.drivingtime;
+                    })
+                    const sortedLocations = resultsArray.map((result) => {
+                        return result.location;
+                    })
+                    resolve(sortedLocations);
+              })
+      });
+  }
+
+  const recalculateRoutes = () => {
+            sortDestinations(destinations).then((sorted) => {
+                sorted.unshift(origin);
+
+                ttapi.services.calculateRoute({
+                    key: apiKey,
+                    locations: sorted
+                })
+                    .then((routeData) => {
+                        const geojson = routeData.toGeoJson();
+                        drawRoute(geojson, map)
+                    })
+            })
+  };
+
+    map.on('click', (e) => {
+        destinations.push(e.lngLat);
+        addDeliveryMarker(e.lngLat, map);
+        recalculateRoutes();
+    })
 
   return () => map.remove();
 }, [longitude, latitude]);
